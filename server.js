9cfcc9b9b0e9800aefeb039443ce8df91749b2d5
Mick Jasinski <m.jasinski@minimalcraft.net>
2018-03-20 10:16:05 +0000

    Make 'app.post' working

--- server.js
+++ server.js
@@ -1,33 +1,50 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const fs  = require('fs');
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
 
 const app = express();
 let stringifyFile = "";
+const home = app.get("/", function(req, res) {
+  res.send("Hello world");
+});
 
 app.use(bodyParser.json());
 
-
-
-app.get('/getNote', function(req, res) {
-  fs.readFile('some.json', 'utf8', function(err, data) {
+app.get("/getNote", function(req, res) {
+  fs.readFile("./some.json", "utf8", function(err, data) {
     if (err) throw err;
-    stringifyFile = data
+    stringifyFile = data;
     res.send(data);
   });
 });
 
-app.post('/updateNote/:note', function(req, res) {
-    stringifyFile = req.param.note;
-    fs.writeFile('./some.json', JSON.stringify(stringifyFile), function(err) {
+// app.post("/updateNote/:note", function(req, res) {
+//   fs.readFile("./some.json", "utf8", function(err, data) {
+//     if (err) throw err;
+//     stringifyFile = JSON.parse(data);
+
+//     fs.writeFile("./some.json", stringifyFile + req.params.note, function(err) {
+//       if (err) throw err;
+//       console.log("file updated");
+//       res.end();
+//     });
+//   });
+// });
+
+app.post("/updateNote/:note", function(req,res) {
+  fs.readFile("./some.json", "utf8", function(err, data) {
+    if (err) throw err;
+    stringifyFile = data + req.params.note;
+
+    fs.writeFile("./some.json", stringifyFile, function(err) {
       if (err) throw err;
-      console.log('file updated');
-      res.end();
+      console.log("file updated");
     });
+  });
+});
+
+app.use(function(req, res, next) {
+  res.status(404).send("Sorry, We fuc*ed up or you're asking for too much...");
 });
 
 app.listen(3000);
-
-app.use(function(req, res, next) {
-  res.status(404).send('Sorry, We fuc*ed up or you\'re asking for too much...')
-});
